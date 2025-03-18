16951eea80ee861a6b504a11d16486aaa98f4390
TracyWafula <tracywafula@gmail.com>
2025-03-18 20:31:15 +0100

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+//define current user
+const currentUser = 'Grass Hopper';
+//string interpolation 
+const welcomeMessage =  `Welcome to Flatbook, ${currentUser}!`; 
+//uppercase method
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//short greeting should display the first character of current user
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
